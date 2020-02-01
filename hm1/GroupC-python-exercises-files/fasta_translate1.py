4b2a1b9967974956348e2cf7e9360f2402c527d7
JialinKang <jkang58@jhu.edu>
2020-02-01 13:06:18 -0500

    add fasta_translate.py

---
+++ hm1/GroupC-python-exercises-files/fasta_translate1.py
@@ -0,0 +1,21 @@
+import sys
+
+def fafile2dict():
+    '''
+    read a single FASTA file (SHH.fa) into a dictionary object
+    and print out the dictionary
+    '''
+    line = sys.stdin.readline()
+    seq = {}
+    while line != '':
+        if line[0] == '>':
+            name = line.replace('\n','')
+            seq[name] = ''
+        else:
+            seq[name] += line.replace('\n','').strip()
+        line = sys.stdin.readline()
+    print(seq)
+
+if __name__ == "__main__":
+    fafile2dict()
+
