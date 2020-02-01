d112882f2b98934a066483750d9f340ed2cf0341
JialinKang <jkang58@jhu.edu>
2020-02-01 16:46:10 -0500

    fasta_translate4.py

--- hm1/GroupC-python-exercises-files/fasta_translate4.py
+++ hm1/GroupC-python-exercises-files/fasta_translate4.py
@@ -1,3 +1,7 @@
+from fasta_translate1 import fafile2dict
+from fasta_translate2 import dna2pro, codonTable2dict
+
+
 def codonHard2dict(table_hard):
     '''
     transform codon_table_hard to dict form
@@ -27,8 +31,42 @@ def codonHard2dict(table_hard):
 
     return codon_dict
 
+def dna2pro(codon, gene, fafilename):
+    '''
+    translate gene FASTA file and print in FASTA format
 
+    Parameter
+    ----------------
+    codon:dict
+    the codon table dictionary form {'codon':'protein'}
+    gene:dict
+    the gene dictionary form {'gene name':'DNA sequence'}
+    fafilename:str
+    the name of protein sequence file
+    ----------------
+    '''
+    f = open(fafilename, 'a')
+    for seq in gene.values():
+        protein = ''
+        codon_error = []
+        for frag in range(0, len(seq), 3):
+            if len(seq)-frag < 3:
+                break
+            else:
+                if seq[frag:frag+3] not in codon.keys():
+                    codon_error.append(seq[frag:frag+3])
+                    continue
+                else:
+                    protein = protein + codon[seq[frag:frag+3]]
+        print(protein)
+        print('error codon:', codon_error)
+        f.write(protein)
+        f.write('\n')
+
+    f.close()
 
 
 if __name__ == "__main__":
-    codonHard2dict('codon_table_hard.txt')
+    codon = codonHard2dict('codon_table_hard.txt')
+    genedict = fafile2dict()
+    dna2pro(codon, genedict, 'genesmess.fa')
